3dfa6eda4bcc06e8fea049899f19e1096fab8c42
justwun <quan20212223@gmail.com>
2025-03-02 17:35:23 +0700

    nothing much

--- import_olist_db.sql
+++ import_olist_db.sql
@@ -113,28 +113,6 @@ SELECT DISTINCT seller_zip_code_prefix
 FROM sellers 
 WHERE seller_zip_code_prefix NOT IN (SELECT geolocation_zip_code_prefix FROM geolocation);
 
-SET SQL_SAFE_UPDATES = 0;
 
-SET FOREIGN_KEY_CHECKS = 0;
 
-DELETE FROM order_items  
-WHERE seller_id IN (SELECT seller_id FROM sellers WHERE seller_zip_code_prefix NOT IN (SELECT geolocation_zip_code_prefix FROM geolocation));
-
-DELETE FROM sellers  
-WHERE seller_zip_code_prefix NOT IN (SELECT geolocation_zip_code_prefix FROM geolocation);
-
-SET FOREIGN_KEY_CHECKS = 1;
-
-SET SQL_SAFE_UPDATES = 1;
--- delete the unmatch foreign key
-SET SQL_SAFE_UPDATES = 0;
-
-SET FOREIGN_KEY_CHECKS = 0;
-
-DELETE FROM customers 
-WHERE customer_zip_code_prefix NOT IN (SELECT geolocation_zip_code_prefix FROM geolocation);
-
-SET FOREIGN_KEY_CHECKS = 1;
-
-SET SQL_SAFE_UPDATES = 1;
 

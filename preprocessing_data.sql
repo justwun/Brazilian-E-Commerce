e45d494b1f435843fd458447f0891d955311d056
justwun <quan20212223@gmail.com>
2025-03-02 17:34:24 +0700

    delete unvalid rows

---
+++ preprocessing_data.sql
@@ -0,0 +1,44 @@
+SET SQL_SAFE_UPDATES = 0;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DELETE FROM order_items  
+WHERE seller_id IN (SELECT seller_id FROM sellers WHERE seller_zip_code_prefix NOT IN (SELECT geolocation_zip_code_prefix FROM geolocation));
+
+DELETE FROM sellers  
+WHERE seller_zip_code_prefix NOT IN (SELECT geolocation_zip_code_prefix FROM geolocation);
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+SET SQL_SAFE_UPDATES = 1;
+-- delete the unmatch foreign key
+SET SQL_SAFE_UPDATES = 0;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DELETE FROM customers 
+WHERE customer_zip_code_prefix NOT IN (SELECT geolocation_zip_code_prefix FROM geolocation);
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+SET SQL_SAFE_UPDATES = 1;
+
+
+
+SET SQL_SAFE_UPDATES = 0;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DELETE FROM orders
+WHERE customer_id NOT IN (SELECT customer_id FROM customers);
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+SET SQL_SAFE_UPDATES = 1;
+
+
+
+
+
+
+
